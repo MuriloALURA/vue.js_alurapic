e6fcbd06a5965f6be00bbf30d75aada0f16b9230
Murilo <murilo.alvess@yahoo.com>
2022-06-27 10:14:33 -0300

    feat: 🛠 API - $http - vueResource

--- alurapic/src/App.vue
+++ alurapic/src/App.vue
@@ -1,13 +1,14 @@
 <template>
-<div>
-  <h1>{{titulo}}</h1>
+  <div>
+    <h1>{{ titulo }}</h1>
 
-  <ul>
-    <li v-for="foto of foto">
-      <img :src="foto.url" :alt="foto.titulo">
-    </li>
-  </ul>
-</div>
+    <ul>
+      <li v-for="foto of fotos">
+        <img :src="foto.url" :alt="foto.titulo">
+      </li>
+    </ul>
+
+  </div>
 </template>
 
 <script>
@@ -16,22 +17,21 @@ export default {
   data() {
 
     return {
-      titulo:'AluraPic',
-      foto: [
-        {
-          url:'https://independente.com.br/wp-content/uploads/2018/01/cachorro-independente.jpg',
-          titulo: 'cachorro'
-        },
-        {
-          url:'https://independente.com.br/wp-content/uploads/2018/01/cachorro-independente.jpg',
-          titulo: 'cachorrao'
-        }
-      ]
+
+      titulo: 'Alurapic', 
+      fotos: []
     }
+  },
+
+  created() {
+
+    this.$http.get('http://localhost:3000/v1/fotos')
+      .then(res => res.json())
+      .then(fotos => this.fotos = fotos);
   }
 }
+
 </script>
 
 <style>
-
 </style>
