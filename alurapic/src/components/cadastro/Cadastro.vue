0034e92c1770837960379350c84d19878bf3bb02
Murilo <murilo.alvess@yahoo.com>
2022-07-04 10:05:08 -0300

    feat: 🛠 configurando VeeValidate

--- alurapic/src/components/cadastro/Cadastro.vue
+++ alurapic/src/components/cadastro/Cadastro.vue
@@ -11,12 +11,16 @@
 
       <div class="controle">
         <label for="titulo">TÍTULO</label>
-        <input id="titulo" autocomplete="off" v-model.lazy="foto.titulo">
+        <input name="titulo" v-model="foto.titulo" id="titulo" autocomplete="off" 
+        v-validate data-vv-rules="required|min:3|max:30" data-vv-as="título">
+         <span class="erro" v-show="errors.has('titulo')">{{ errors.first('titulo') }}</span>
       </div>
 
       <div class="controle">
         <label for="url">URL</label>
-        <input id="url" autocomplete="off" v-model.lazy="foto.url">
+        <input name="url" v-model="foto.url" id="url" autocomplete="off"
+        v-validate data-vv-rules="required">
+        <span class="erro" v-show="errors.has('url')">{{ errors.first('url') }}</span>
         <imagem-responsiva v-show="foto.url" :url="foto.url" :titulo="foto.titulo"/>
       </div>
 
@@ -62,13 +66,18 @@ export default {
   methods: {
 
       grava() {
-
-          this.service
-            .cadastra(this.foto)
-            .then(() => {
-              if(this.id) this.$router.push({ name: 'home' });
-              this.foto = new Foto();
-            }, err => console.log(err));
+        this.validator
+          .validateAll()
+          .then(success =>{
+            if (success) {
+              this.service
+                .cadastra(this.foto)
+                .then(() => {
+                  if(this.id) this.$router.push({ name: 'home' });
+                  this.foto = new Foto();
+                }, err => console.log(err));              
+            }
+          })
       }
   },
 
@@ -108,5 +117,9 @@ export default {
 
   .centralizado {
     text-align: center;
+  }
+
+  .erro{
+    color: red;
   }
 </style>
