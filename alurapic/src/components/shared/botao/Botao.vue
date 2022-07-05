dd9832cef1bf758a3bba7fb9055fa0cfbcd0e903
Murilo <murilo.alvess@yahoo.com>
2022-07-05 13:51:12 -0300

    feat: 🛠 node-sass, jquery e webpack

--- alurapic/src/components/shared/botao/Botao.vue
+++ alurapic/src/components/shared/botao/Botao.vue
@@ -49,23 +49,8 @@ export default {
 }
 </script>
 
-<style>
+<style scoped lang="sass">
 
-    .botao {
-        display: inline-block;
-        padding: 10px;
-        border-radius: 3px;
-        margin: 10px;
-        font-size: 1.2em;
-    }
+    @import './Botao.scss';
 
-    .botao-perigo {
-        background: $cor;
-        color: white;
-    }
-
-    .botao-padrao {
-        background: darkcyan;
-        color: white;
-    }
 </style>
